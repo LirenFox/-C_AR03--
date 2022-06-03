4eb77526b2f76861d4cec345d01b096b42fd6164
LirenFox <104010604+LirenFox@users.noreply.github.com>
2022-06-03 18:08:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int sum=0;
-    for(int i=0; i<6; i++){
+    for(int i=0; i<6; i++){//六個數的立方和
         int temp;
         cin >> temp;
         sum+=temp*temp*temp;
